652c4bce7adc6cad9c8d700adaf115da856348f4
Jordan Leigh <alwaysbcodingeth@Jordans-Mac-mini.local>
2017-12-09 01:43:39 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var xmrInstance = require("./lib/monero/xmrInstance")
+
+global.jssha3 = require("js-sha3")
+global.lodash = require("lodash")
+global.crypto = xmrInstance
+
+class XMR {
+  constructor({seed}) {
+    var first, second, spendKeys, viewKeys
+
+    if(seed.length !== 64) {
+      first = global.crypto.cn_fast_hash(seed)
+    } else {
+      first = global.crypto.sc_reduce32(seed)
+    }
+
+    spendKeys = global.crypto.generate_keys(first)
+    second = global.crypto.cn_fast_hash(first)
+    viewKeys = global.crypto.generate_keys(second)
+
+    this.keys = {
+      spend: spendKeys,
+      view: viewKeys
+    }
+    this.address = global.crypto.pubkeys_to_string(spendKeys.pub, viewKeys.pub)
+  }
+}
+
+global.XMR = XMR
+
+require("repl").start()
